b893e4dbbdb6138a9f4cd861359b5e04429e5f9c
baki <baki@192.168.1.103>
2025-12-30 21:26:20 +0300

    ADC.h

--- BaremetalF410rbGit/Inc/adc.h
+++ BaremetalF410rbGit/Inc/adc.h
@@ -8,6 +8,24 @@
 #ifndef ADC_H_
 #define ADC_H_
 
+#include "baseAdress.h"
 
+#define ADC_SR_OFFSET  0x00
+#define ADC_SR	(*(volatile unsigned int *)(ADC1_PERIPHERAL +ADC_SR_OFFSET))
+
+#define ADC_CR2_OFFSET 0x08
+#define ADC_SR	(*(volatile unsigned int *)(ADC1_PERIPHERAL +ADC_CR2_OFFSET))
+
+#define ADC_SQR1_OFFSET  0x2C
+#define ADC_SQR1		(*(volatile unsigned int *)(ADC1_PERIPHERAL +ADC_SQR1_OFFSET))
+
+#define ADC_SQR3_OFFSET  0x34
+#define ADC_SQR3		(*(volatile unsigned int *)(ADC1_PERIPHERAL +ADC_SQR3_OFFSET))
+
+
+#define ADC_SR_EOC 		(1<<2)
+#define ADC_CR2_ADON	(1<<0)
+#define ADC_CR2_SWSTART (1<<30)
+#define ADC_CR2_CONT	(1<<1)
 
 #endif /* ADC_H_ */
